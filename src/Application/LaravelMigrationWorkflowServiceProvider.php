79aae7a030e0400400b4f24dc9297260eb7bc696
José Espinoza Salas <j.espinoza@innlab.cl>
2019-10-18 09:32:42 -0300

    initial structure

---
+++ src/Application/LaravelMigrationWorkflowServiceProvider.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace MrCoto\MigrationWorkflow\Application;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMigrationWorkflowServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Publish configuration file
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // TODO: Fill me with code if needed
+    }
+
+    /**
+     * Make config publishment optional by merge the config from the package
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Fill me with code if needed
+    }
+
+}
