27646fd8544d3c1f329a022132be1dad5eb99689
José Espinoza Salas <j.espinoza@innlab.cl>
2019-10-18 16:10:46 -0300

    Config data in phpunit.xml

--- tests/LaravelTest.php
+++ tests/LaravelTest.php
@@ -14,13 +14,6 @@ abstract class LaravelTest extends TestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        $default = 'pgsql';
-        $app['config']->set("database.default", $default);
-        $app['config']->set("database.connections.$default.host", '127.0.0.1');
-        $app['config']->set("database.connections.$default.port", '5432');
-        $app['config']->set("database.connections.$default.database", 'migration_workflow');
-        $app['config']->set("database.connections.$default.username", 'postgres');
-        $app['config']->set("database.connections.$default.password", 'postgres');
     }
 
 }
