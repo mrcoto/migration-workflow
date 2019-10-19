1ffa51177b07460b038857974abcb727f3af471a
José Espinoza <espinozasalas.jose@gmail.com>
2019-10-19 14:16:18 -0300

    added migration handler domain test

---
+++ tests/Domain/MigrationWorkflowHandlerDomainTest.php
@@ -0,0 +1,39 @@
+<?php 
+
+namespace MrCoto\MigrationWorkflow\Test\Domain;
+
+use MrCoto\MigrationWorkflow\Domain\Handlers\MigrationWorkflowHookHandler;
+use MrCoto\MigrationWorkflow\Domain\Handlers\MigrationWorkflowHandler;
+use MrCoto\MigrationWorkflow\Domain\Logger\Logger;
+use MrCoto\MigrationWorkflow\Infrastructure\Handlers\Eloquent\MigrationEloquentStepHandler;
+use MrCoto\MigrationWorkflow\Test\Stub\Workflow\DummyWorkflow;
+use PHPUnit\Framework\TestCase;
+
+class MigrationWorkflowHandlerDomainTest extends TestCase
+{
+
+    public function test_should_handle_workflow()
+    {
+        $loggerMock = $this->getMockBuilder(Logger::class)->getMock();
+        $migrationHandlerMock = $this->getMockBuilder(MigrationEloquentStepHandler::class)->getMock();
+        $seedHandlerMock = $this->getMockBuilder(MigrationEloquentStepHandler::class)->getMock();
+        $hookHandlerMock = $this->getMockBuilder(MigrationWorkflowHookHandler::class)->getMock();
+        $workflowHandler = new MigrationWorkflowHandler(
+            $loggerMock,
+            $migrationHandlerMock,
+            $seedHandlerMock,
+            $hookHandlerMock
+        );
+
+        $hookHandlerMock->expects($this->exactly(1))->method('beforeAll');
+        $hookHandlerMock->expects($this->exactly(1))->method('afterAll');
+
+        $seedHandlerMock->expects($this->exactly(1))->method('handle');
+        $migrationHandlerMock->expects($this->exactly(2))->method('handle');
+
+        $workflowHandler->handle(
+            new DummyWorkflow
+        );
+    }
+
+}
